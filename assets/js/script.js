1296d3c2b61ecc643f9a20f2ba8bb95c1f4a434b
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2020-12-21 17:12:09 -0500

    rename variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var addItem = function() {
-    var newItemEl = document.createElement("li");
-    newItemEl.className = "task-item";
-    newItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(newItemEl);
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
 };
 
-buttonEl.addEventListener("click", addItem);
+buttonEl.addEventListener("click", createTaskHandler);
